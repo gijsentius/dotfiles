fbfe533f20a2bfb5522337c6bf7cd646b8e39cc5
Gijs Entius <g.m.entius@gmail.com>
2019-03-01 10:06:07 +0100

    init

---
+++ install.sh
@@ -0,0 +1,15 @@
+# Install i3-gaps
+cd /path/where/you/want/the/repository
+
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
+
+
