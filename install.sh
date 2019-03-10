9b897fd9780a1b5da2edadfd9572e8d28b62ddd9
Gijs Entius <g.m.entius@gmail.com>
2019-03-10 17:57:27 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-# Install i3-gaps
-cd /path/where/you/want/the/repository
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Install i3-gaps
+cd ~/Downloads 
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 
@@ -12,6 +13,12 @@ mkdir -p build && cd build/
 make
 sudo make install
 
+cd ${DIR}
+
 sudo apt install i3status i3lock
 sudo cp i3/i3status.conf /etc/i3status.conf
 sudo cp i3/config ~/.config/i3/config
+
+sudo apt install neovim
+
+
