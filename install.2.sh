32f9e3832027222b66bff7d754b9d66266a2d3f4
Gijs Entius <g.m.entius@gmail.com>
2019-08-15 21:55:05 +0200

    cleaned up the install script

--- install.2.sh
+++ install.2.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
+# install all necessary software
 sudo add-apt-repository ppa:kgilmer/speed-ricer
-
 sudo apt update
+sudo apt install suckless-tools thunar alsa-utils xbacklight feh lxappearance gtk-chtheme qt4-qtconfig i3status net-tools gucharmap git i3lock gpick polybar i3-wm polybar
 
-sudo apt install i3-wm
-
+# experimental
+sudo apt install fonts-font-awesome
