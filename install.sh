15bc2e8b1b5a84ecc9f6eb692633d9dcfb66b992
Gijs Entius <g.m.entius@gmail.com>
2019-08-16 16:32:53 +0200

    added rofi themes

--- install.sh
+++ install.sh
@@ -2,13 +2,34 @@
 
 # install all necessary software
 sudo add-apt-repository ppa:kgilmer/speed-ricer
+sudo add-apt-repository ppa:papirus/papirus
+sudo add-apt-repository ppa:noobslab/themes
+
 sudo apt update
-sudo apt install suckless-tools thunar alsa-utils xbacklight feh lxappearance gtk-chtheme qt4-qtconfig i3status net-tools gucharmap git i3lock gpick polybar i3-wm polybar
+## general tools
+sudo apt install suckless-tools thunar alsa-utils xbacklight feh lxappearance gtk-chtheme qt4-qtconfig i3status net-tools gucharmap git i3lock gpick vim
+## icon theme and gtk theme
+sudo apt install arc-theme papirus-icon-theme
+## convert images
+sudo apt install imagemagick
+## i3 and polybar
+sudo apt install polybar i3-wm
 
 # experimental
+## solution for font problems
 sudo apt install fonts-font-awesome
-# change to temp directory
 git clone https://github.com/stark/siji && cd siji
 ./install.sh
+### remove the siji files
 cd ..
 rm -rf siji
+## install rofi themes
+mkdir -p ~/.config/rofi
+### rofi -dump-config > ~/.config/rofi/config.rasi
+cp rofi/config.rasi ~/.config/rofi/config.rasi
+### copy themes
+mkdir -p ~/.config/rofi/themes
+cp rofi/themes/* ~/.config/rofi/themes/
+### copy icons
+mkdir -p ~/.icons
+cp -r /usr/share/icons/* ~/.icons/
