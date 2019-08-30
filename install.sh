ac9725cac98abbe6f7854c3418ccfe259b426f50
Gijs Entius <g.m.entius@gmail.com>
2019-08-30 23:49:16 +0200

    Update install.sh
    added curl

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository ppa:papirus/papirus -y
 sudo add-apt-repository ppa:noobslab/themes -y
 sudo apt update
 ## general tools
-sudo apt -y install suckless-tools thunar alsa-utils xbacklight feh lxappearance gtk-chtheme qt4-qtconfig i3status net-tools gucharmap git i3lock gpick vim bc xclip xdotool xsel
+sudo apt -y install suckless-tools curl thunar alsa-utils xbacklight feh lxappearance gtk-chtheme qt4-qtconfig i3status net-tools gucharmap git i3lock gpick vim bc xclip xdotool xsel
 ## icon theme and gtk theme
 sudo apt -y install arc-theme papirus-icon-theme
 ## convert images
