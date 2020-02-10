70864390012252c0114e990c0a023e8c1d86c0ae
Gijs Entius <gentius@gentius-pc.quintor.local>
2020-02-10 10:49:13 +0100

    update van install script

--- install.sh
+++ install.sh
@@ -8,15 +8,18 @@ sudo add-apt-repository ppa:papirus/papirus -y
 sudo add-apt-repository ppa:noobslab/themes -y
 sudo apt update
 ## general tools
-sudo apt -y install suckless-tools tree  curl thunar alsa-utils xbacklight feh lxappearance gtk-chtheme qt4-qtconfig i3status net-tools gucharmap git i3lock gpick vim bc xclip xdotool xsel
+sudo apt -y install suckless-tools tree  curl thunar alsa-utils xbacklight feh lxappearance gtk-chtheme qt4-qtconfig i3status net-tools gucharmap git i3lock gpick vim bc xclip xdotool xsel python3-pip git
 ## icon theme and gtk theme
 sudo apt -y install arc-theme papirus-icon-theme
 ## convert images
 sudo apt -y install imagemagick
 ## i3, polybar and rofi
-sudo apt -y install polybar i3-gaps-wm rofi
+sudo apt -y install polybar rofi
 ## password manager
 sudo apt -y install pass
+## pandoc etc
+sudo apt -y install pandoc pandoc-citeproc
+
 -
 # experimental
 ## solution for font problems
