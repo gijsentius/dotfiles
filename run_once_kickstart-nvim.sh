7396c93dc25439db0b2beb59d4b306ad9741eba1
Gijs Entius <g.m.entius@gmail.com>
2024-09-16 19:47:14 +0200

    feat: Add nvim and tmux setup

--- run_once_kickstart-nvim.sh
+++ run_once_kickstart-nvim.sh
@@ -1,1 +1,5 @@
-git clone https://github.com/gijsentius/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+#!/bin/sh
+
+if [ ! -d "${XDG_CONFIG_HOME:-$HOME/.config}/nvim" ]; then
+  git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+fi
