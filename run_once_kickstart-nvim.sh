04a2be445351ae22d3c9f2ab3d73ecc8a582f8c9
Gijs Entius <g.m.entius@gmail.com>
2024-10-01 21:28:19 +0200

    feat: Add brew install

--- run_once_kickstart-nvim.sh
+++ run_once_kickstart-nvim.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ ! -d "${XDG_CONFIG_HOME:-$HOME/.config}/nvim" ]; then
-  git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+  git clone https://github.com/gijsentius/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 fi
